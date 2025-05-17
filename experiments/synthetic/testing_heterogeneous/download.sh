e571d7f65d08c1b1d94fcb9eb11511707024431a
pmgblz <33529882+pmgblz@users.noreply.github.com>
2025-05-16 17:15:36 -0700

    remove path

--- experiments/synthetic/testing_heterogeneous/download.sh
+++ experiments/synthetic/testing_heterogeneous/download.sh
@@ -2,4 +2,4 @@
 
 mkdir -p results
 
-rsync -auv sesia@discovery.usc.edu:/home1/sesia/Workspace/randomized_experiments/code/experiments/testing_heterogeneous/results/ results/
+rsync -auv "insertpath"
--- experiments/synthetic/testing_transfer/download.sh
+++ experiments/synthetic/testing_transfer/download.sh
@@ -2,4 +2,4 @@
 
 mkdir -p results
 
-rsync -auv sesia@discovery.usc.edu:/home1/sesia/Workspace/randomized_experiments/code/experiments/testing_transfer/results/ results/
+rsync -auv "insertpath"
